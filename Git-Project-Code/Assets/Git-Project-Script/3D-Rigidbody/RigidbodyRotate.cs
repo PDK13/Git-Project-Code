62cbe4335f5af385abd519f7cc20c9b052f9f9d2
Khoa Pham Dang <khoa199960@gmail.com>
2022-08-30 14:31:40 +0700

    Update

--- Git-Project-Code/Assets/Git-Project-Script/3D-Rigidbody/RigidbodyRotate.cs
+++ Git-Project-Code/Assets/Git-Project-Script/3D-Rigidbody/RigidbodyRotate.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RigidbodyVelocity : MonoBehaviour
+public class RigidbodyRotate : MonoBehaviour
 {
-    [SerializeField] [Tooltip("Vector Right - Red Axis")] private bool b_Rigidbody_X = true;
+    [SerializeField] [Tooltip("Vector Right - Red Axis")] private bool b_Follow_Right = true;
 
-    [SerializeField] [Tooltip("Vector Forward - Blue Axis")] private bool b_Rigidbody_Z = false;
+    [SerializeField] [Tooltip("Vector Forward - Blue Axis")] private bool b_Follow_Forward = false;
 
-    [SerializeField] [Tooltip("Vector Up - Green Axis")] private bool b_Rigidbody_Y = false;
+    [SerializeField] [Tooltip("Vector Up - Green Axis")] private bool b_Follow_Up = false;
 
     private Rigidbody com_Rigidbody;
 
@@ -35,17 +35,17 @@ public class RigidbodyVelocity : MonoBehaviour
     {
         if (com_Rigidbody != null)
         {
-            if (b_Rigidbody_X)
+            if (b_Follow_Right)
             {
                 this.transform.right = new Vector3(com_Rigidbody.velocity.x, com_Rigidbody.velocity.y, com_Rigidbody.velocity.z);
             }
 
-            if (b_Rigidbody_Y)
+            if (b_Follow_Up)
             {
                 this.transform.up = new Vector3(com_Rigidbody.velocity.x, com_Rigidbody.velocity.y, com_Rigidbody.velocity.z);
             }
 
-            if (b_Rigidbody_Z)
+            if (b_Follow_Forward)
             {
                 this.transform.forward = new Vector3(com_Rigidbody.velocity.x, com_Rigidbody.velocity.y, com_Rigidbody.velocity.z);
             }
@@ -53,50 +53,50 @@ public class RigidbodyVelocity : MonoBehaviour
         else
         if (com_Rigidbody2D != null)
         {
-            if (b_Rigidbody_X)
+            if (b_Follow_Right)
             {
                 this.transform.right = new Vector3(com_Rigidbody2D.velocity.x, com_Rigidbody2D.velocity.y);
             }
 
-            if (b_Rigidbody_Y)
+            if (b_Follow_Up)
             {
                 this.transform.up = new Vector3(com_Rigidbody2D.velocity.x, com_Rigidbody2D.velocity.y);
             }
 
-            if (b_Rigidbody_Z)
+            if (b_Follow_Forward)
             {
                 this.transform.forward = new Vector3(com_Rigidbody2D.velocity.x, com_Rigidbody2D.velocity.y);
             }
         }
     }
 
-    public void Set_Rigidbody_X(bool b_Rigidbody_X)
+    public void Set_Follow_Right(bool b_Follow_Right)
     {
-        this.b_Rigidbody_X = b_Rigidbody_X;
+        this.b_Follow_Right = b_Follow_Right;
     }
 
-    public bool Get_Rigidbody_X()
+    public bool Get_Follow_Right()
     {
-        return this.b_Rigidbody_X;
+        return this.b_Follow_Right;
     }
 
-    public void Set_Rigidbody_Y(bool b_Rigidbody_Y)
+    public void Set_Follow_Up(bool b_Follow_Up)
     {
-        this.b_Rigidbody_Y = b_Rigidbody_Y;
+        this.b_Follow_Up = b_Follow_Up;
     }
 
-    public bool Get_Rigidbody_Y()
+    public bool Get_Follow_Up()
     {
-        return this.b_Rigidbody_Y;
+        return this.b_Follow_Up;
     }
 
-    public void Set_Rigidbody_Z(bool b_Rigidbody_Z)
+    public void Set_Follow_Forward(bool b_Follow_Forward)
     {
-        this.b_Rigidbody_Z = b_Rigidbody_Z;
+        this.b_Follow_Forward = b_Follow_Forward;
     }
 
-    public bool Get_Rigidbody_Z()
+    public bool Get_Follow_Forward()
     {
-        return this.b_Rigidbody_Z;
+        return this.b_Follow_Forward;
     }
 }
