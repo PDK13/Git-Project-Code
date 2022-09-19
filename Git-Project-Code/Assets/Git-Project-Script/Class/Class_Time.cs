f5034ac3eeb0c3f695691f79266aedace7934824
Khoa Pham Dang <khoa199960@gmail.com>
2022-09-19 12:31:49 +0700

    Update Class_Time.cs

--- Git-Project-Code/Assets/Git-Project-Script/Class/Class_Time.cs
+++ Git-Project-Code/Assets/Git-Project-Script/Class/Class_Time.cs
@@ -13,25 +13,69 @@ public class Class_Time
 [System.Serializable]
 public class Class_Time_Value
 {
-    [SerializeField] private int i_Second = 0;
+    [SerializeField] private int i_Hour = 0;
 
     [SerializeField] private int i_Minute = 0;
 
-    [SerializeField] private int i_Hour = 0;
+    [SerializeField] private int i_Second = 0;
 
-    public void Set_Time_Second(int i_Second)
+    #region Hour
+
+    public void Set_Time_Hour(int i_Hour)
     {
-        this.i_Second = i_Second;
+        if (i_Hour < 0)
+        {
+            this.i_Hour = 0;
+        }
+        else
+        {
+            this.i_Hour = i_Hour;
+        }
     }
 
-    public int Get_Time_Second()
+    public void Set_Time_Hour_Chance(int i_Hour_Chance)
     {
-        return i_Second;
+        if (i_Hour + i_Hour_Chance < 0)
+        {
+            this.i_Hour = 0;
+        }
+        else
+        {
+            this.i_Hour += i_Hour_Chance;
+        }
     }
+
+    public int Get_Time_Hour()
+    {
+        return i_Hour;
+    }
+
+    #endregion
+
+    #region Minute
 
     public void Set_Time_Minute(int i_Minute)
     {
-        this.i_Minute = i_Minute;
+        if (i_Minute < 0)
+        {
+            this.i_Minute = 0;
+        }
+        else
+        {
+            this.i_Minute = i_Minute;
+        }
+    }
+
+    public void Set_Time_Minute_Chance(int i_Minute_Chance)
+    {
+        if (i_Minute + i_Minute_Chance < 0)
+        {
+            this.i_Minute = 0;
+        }
+        else
+        {
+            this.i_Minute += i_Minute_Chance;
+        }
     }
 
     public int Get_Time_Minute()
@@ -39,13 +83,38 @@ public class Class_Time_Value
         return i_Minute;
     }
 
-    public void Set_Time_Hour(int i_Hour)
+    #endregion
+
+    #region Second
+
+    public void Set_Time_Second(int i_Second)
     {
-        this.i_Hour = i_Hour;
+        if (i_Second < 0)
+        {
+            this.i_Second = 0;
+        }
+        else
+        {
+            this.i_Second = i_Second;
+        }
     }
 
-    public int Get_Time_Hour()
+    public void Set_Time_Second_Chance(int i_Second_Chance)
     {
-        return i_Hour;
+        if (i_Second + i_Second_Chance < 0)
+        {
+            this.i_Second = 0;
+        }
+        else
+        {
+            this.i_Second += i_Second_Chance;
+        }
     }
+
+    public int Get_Time_Second()
+    {
+        return i_Second;
+    }
+
+    #endregion
 }
