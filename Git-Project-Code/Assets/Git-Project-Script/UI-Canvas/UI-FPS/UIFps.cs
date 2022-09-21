b8c239176478d197688f3e6ba551fb09e2957c1c
Khoa Pham Dang <80452561+PDK13@users.noreply.github.com>
2022-09-21 19:12:00 +0700

    Add FPS

---
+++ Git-Project-Code/Assets/Git-Project-Script/UI-Canvas/UI-FPS/UIFps.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+public class UIFps : MonoBehaviour
+{
+    [SerializeField] private bool b_Time_Delay = false;
+
+    [SerializeField] private float f_Time_Delay = 0.1f;
+
+    private float f_DeltaTime = 0.0f;
+    private float f_Fps = 0.0f;
+
+    private void Start()
+    {
+        StartCoroutine(Set_Test());
+    }
+
+    private IEnumerator Set_Test()
+    {
+        yield return null;
+
+        do
+        {
+            f_DeltaTime += Time.deltaTime;
+            f_DeltaTime /= 2.0f;
+            f_Fps = 1.0f / f_DeltaTime;
+
+            if (GetComponent<Text>())
+            {
+                GetComponent<Text>().text = "FPS: " + ((int)f_Fps).ToString();
+            }
+            else
+            if (GetComponent<TextMeshProUGUI>())
+            {
+                GetComponent<TextMeshProUGUI>().text = "FPS: " + ((int)f_Fps).ToString();
+            }
+            
+            if (b_Time_Delay)
+            {
+                yield return null;
+            }
+            else
+            {
+                yield return new WaitForSeconds(f_Time_Delay);
+            }
+        }
+        while (1 == 1);
+    }
+}
