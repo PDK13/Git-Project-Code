bfb981e2a9105f79395e58f029039d2a112268df
Khoa Pham Dang <80452561+PDK13@users.noreply.github.com>
2022-09-29 14:08:31 +0700

    Add Simple-Coroutine

--- Git-Project-Code/Assets/Git-Project-Script/Simple-C#/Simple-Coroutine/Simple_Coroutine.cs
+++ Git-Project-Code/Assets/Git-Project-Script/Simple-C#/Simple-Coroutine/Simple_Coroutine.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Simple_Coroutine : MonoBehaviour
 {
+    private Coroutine m_Coroutine;
+
     private void Start()
     {
         StartCoroutine(Set_Coroutne_1());
         StartCoroutine(Set_Coroutne_2());
         StartCoroutine(Set_Coroutne_3());
+
+        m_Coroutine = StartCoroutine(Set_Coroutine_Final());
     }
 
     private IEnumerator Set_Coroutne_1()
@@ -16,7 +20,7 @@ public class Simple_Coroutine : MonoBehaviour
         //Skip 1 frame
         yield return null;
 
-        Debug.LogFormat("{0}: Coroutine 1");
+        Debug.LogFormat("{0}: Coroutine 1 Done!", name);
     }
 
     private IEnumerator Set_Coroutne_2()
@@ -24,7 +28,7 @@ public class Simple_Coroutine : MonoBehaviour
         yield return null;
         yield return null;
 
-        Debug.LogFormat("{0}: Coroutine 2");
+        Debug.LogFormat("{0}: Coroutine 2 Done!", name);
     }
 
     private IEnumerator Set_Coroutne_3()
@@ -33,6 +37,30 @@ public class Simple_Coroutine : MonoBehaviour
         yield return null;
         yield return null;
 
-        Debug.LogFormat("{0}: Coroutine 3");
+        Debug.LogFormat("{0}: Coroutine 3 Done!", name);
+
+        yield return m_Coroutine;
+
+        Debug.LogFormat("{0}: All Coroutine End!", name);
+    }
+
+    private IEnumerator Set_Coroutine_Final()
+    {
+        yield return null;
+        yield return null;
+        yield return null;
+        yield return null;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.LogFormat("{0}: Coroutine Final End in {1}!", name, 10 - i);
+
+            yield return new WaitForSeconds(1f);
+        }
+
+        //Or Use this code instead... :)
+        //yield return new WaitForSeconds(10f);
+
+        Debug.LogFormat("{0}: Coroutine Final Done!", name);
     }
 }
