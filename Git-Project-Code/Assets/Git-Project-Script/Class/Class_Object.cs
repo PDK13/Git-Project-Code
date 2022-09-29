7001194ec281ad1c2cbbbe5f34d2f84118f380ed
Khoa Pham Dang <80452561+PDK13@users.noreply.github.com>
2022-09-29 19:08:44 +0700

    Update Class_Object.cs

--- Git-Project-Code/Assets/Git-Project-Script/Class/Class_Object.cs
+++ Git-Project-Code/Assets/Git-Project-Script/Class/Class_Object.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 /// </summary>
 public class Class_Object
 {
-    #region GameObject 
+    #region ================================================================== GameObject 
 
     #region Create
 
@@ -44,23 +44,40 @@ public class Class_Object
     //BEWARD:
     //All content(s) in folder(s) "Resources" will be builded to Application, even they mightn't be used in Build-Game Application
 
-    public static List<GameObject> Get_Resources_Prefab(string s_Path_Folder)
+    public static List<GameObject> Get_Resources_Prefab(string s_Path_inResources)
     {
-        GameObject[] l_Prefab_Array = Resources.LoadAll<GameObject>(Class_String.Get_String_Replace_Resources(s_Path_Folder));
+        GameObject[] l_Prefab_Array = Resources.LoadAll<GameObject>(Class_String.Get_String_Replace_Resources(s_Path_inResources));
         List<GameObject> l_Prefab_List = new List<GameObject>();
         l_Prefab_List.AddRange(l_Prefab_Array);
         return l_Prefab_List;
     }
 
-    public static List<Sprite> Get_Resources_Sprite(string s_Path_Folder)
+    public static List<Sprite> Get_Resources_Sprite(string s_Path_inResources)
     {
-        Sprite[] l_Sprite_Array = Resources.LoadAll<Sprite>(Class_String.Get_String_Replace_Resources(s_Path_Folder));
+        Sprite[] l_Sprite_Array = Resources.LoadAll<Sprite>(Class_String.Get_String_Replace_Resources(s_Path_inResources));
         List<Sprite> l_Sprite_List = new List<Sprite>();
         l_Sprite_List.AddRange(l_Sprite_Array);
         return l_Sprite_List;
     }
 
+    public static List<TextAsset> Get_Resources_TextAsset(string s_Path_inResources)
+    {
+        TextAsset[] l_TextAsset_Array = Resources.LoadAll<TextAsset>(Class_String.Get_String_Replace_Resources(s_Path_inResources));
+        List<TextAsset> l_TextAsset_List = new List<TextAsset>();
+        l_TextAsset_List.AddRange(l_TextAsset_Array);
+        return l_TextAsset_List;
+    }
+
     #endregion
+
+    #endregion
+
+    #region ================================================================== Object
+
+    public static bool Get_Object_isType<Type>(object m_Object)
+    {
+        return m_Object is Type;
+    }
 
     #endregion
 }
