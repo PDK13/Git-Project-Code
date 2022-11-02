b989a1e2daa8bcf0f9687a3a47b544b2f906e537
Khoa Pham Dang <khoa199960@gmail.com>
2022-11-02 12:06:29 +0700

    UPdate Lab

---
+++ Git-Project-Code/Assets/Git-Project-Script/-/Lab/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Sprite m_Sprite;
+
+    public SpriteRenderer m_SpriteRenderer;
+
+    public Camera m_Camera;
+
+    public Canvas m_Canvas;
+
+    private void Start()
+    {
+        
+    }
+}
