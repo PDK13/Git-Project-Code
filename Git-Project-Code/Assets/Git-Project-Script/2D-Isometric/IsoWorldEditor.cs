06d5df343d0f212040d7d324066fb07a90aa187c
Khoa Pham Dang <khoa199960@gmail.com>
2022-11-11 09:29:34 +0700

    Update IsoWorldEditor.cs

--- Git-Project-Code/Assets/Git-Project-Script/2D-Isometric/IsoWorldEditor.cs
+++ Git-Project-Code/Assets/Git-Project-Script/2D-Isometric/IsoWorldEditor.cs
@@ -56,6 +56,8 @@ public class IsoWorldEditor : EditorWindow
             {
                 m_IsoWorldManager.SetWorldManagerStatic();
 
+                GUILayout.Space(10);
+
                 GUILayout.Label("WORLD EDITOR", m_StyleLabel);
 
                 //Editor!
@@ -90,8 +92,6 @@ public class IsoWorldEditor : EditorWindow
             alignment = TextAnchor.MiddleCenter,
         };
 
-        GUILayout.Space(10);
-
         GUILayout.Label("BLOCKS", m_Style);
 
         GUILayout.BeginHorizontal();
@@ -109,14 +109,54 @@ public class IsoWorldEditor : EditorWindow
 
         if (IsoWorldManager.Blocks.Count > 0)
         {
-            int m_HorizontalCount = 0;
+            GUILayout.BeginVertical();
 
-            for (int i = 0; i < IsoWorldManager.Blocks.Count; i++) 
+            int m_Index = m_Page * c_BlockHorizontalMax * c_BlockVerticallMax;
+
+            float m_Width = position.width / c_BlockHorizontalMax - 4f;
+            float m_Height = m_Width;
+
+            for (int i = 0; i < c_BlockVerticallMax; i++)
             {
-                m_HorizontalCount++;
+                GUILayout.BeginHorizontal();
 
+                for (int j = 0; j < c_BlockHorizontalMax; j++)
+                {
+                    if (m_Index > IsoWorldManager.Blocks.Count - 1)
+                    {
+                        GUILayout.Button("...", GUILayout.Width(m_Width), GUILayout.Height(m_Height));
+                    }
+                    else
+                    {
+                        Sprite m_Sprite = IsoWorldManager.Blocks[m_Index].GetComponent<SpriteRenderer>().sprite;
 
+                        //var croppedTexture = new Texture2D((int)m_Sprite.rect.width, (int)m_Sprite.rect.height);
+                        //var pixels = m_Sprite.texture.GetPixels((int)m_Sprite.textureRect.x,
+                        //                                         (int)m_Sprite.textureRect.y,
+                        //                                         (int)m_Sprite.textureRect.width,
+                        //                                         (int)m_Sprite.textureRect.height);
+                        //croppedTexture.SetPixels(pixels);
+                        //croppedTexture.Apply();
+
+                        //GUIContent m_Content = new GUIContent((Texture)croppedTexture);
+
+                        GUIContent m_Content = new GUIContent(m_Sprite.associatedAlphaSplitTexture);
+
+                        //GUILayout.Button("", GUILayout.Width(m_Width), GUILayout.Height(m_Height));
+                        GUILayout.Button(m_Content, GUILayout.Width(m_Width), GUILayout.Height(m_Height));
+                    }
+
+                    m_Index++;
+                }
+
+                GUILayout.EndHorizontal();
             }
+
+            GUILayout.EndVertical();
+        }
+        else 
+        {
+            m_Page = 0;
         }
     }
 
